6d501f4380caad4e0677c89bbda71fdf1d5d7524
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-21 22:10:36 +0100

    Initial commit

---
+++ APOD/APOD/Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APOD
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ APOD/APOD/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  APOD
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
