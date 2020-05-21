353d2788ad68dd8009d49147ccadb6f7f45732ce
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-21 23:40:41 +0100

    Picture of the day header model set up

---
+++ APOD/APOD/Models/OTKPod.h
@@ -0,0 +1,27 @@
+//
+//  OTKPod.h
+//  APOD
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OTKPod : NSObject
+
+@property (nonatomic) NSDate *date;
+@property (nonatomic, copy) NSString * explanation;
+@property (nonatomic) NSURL *imageURL;
+@property (nonatomic, copy) NSString *title;
+
+- (instancetype)initWithDate:(NSDate *)date
+                 explanation:(NSString *)explanation
+                    imageURL:(NSURL *)imageURL
+                       title:(NSString *)title;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ APOD/APOD/Models/OTKPod.m
@@ -0,0 +1,13 @@
+//
+//  OTKPod.m
+//  APOD
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import "OTKPod.h"
+
+@implementation OTKPod
+
+@end
