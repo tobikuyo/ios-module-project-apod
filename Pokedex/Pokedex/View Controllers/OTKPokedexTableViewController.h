ce2d664b2d5fbbd00da48c9d0a9470892b3b0e09
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-22 16:52:14 +0100

    View controllers created and outlets connected

---
+++ Pokedex/Pokedex/View Controllers/OTKPokedexTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  OTKPokedexTableViewController.h
+//  Pokedex
+//
+//  Created by Tobi Kuyoro on 22/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OTKPokedexTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
